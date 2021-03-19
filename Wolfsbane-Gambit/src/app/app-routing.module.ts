0dea138e60b05ee2511f3e6441f6a2ed091caa1d
EvoShady <danc1_cosmin@yahoo.com>
2021-03-19 14:06:04 +0200

    Added afterIntroTestPage, IntroPage moved, added routing and Faded-Animation

--- Wolfsbane-Gambit/src/app/app-routing.module.ts
+++ Wolfsbane-Gambit/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AfterIntroTestPageComponent } from './after-intro-test-page/after-intro-test-page.component'
+import { IntroPageComponent } from './intro-page/intro-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/AppComponent', pathMatch: 'full' },
+  { path: 'introPage', component: IntroPageComponent},
+  { path : 'afterIntro', component: AfterIntroTestPageComponent, data: {animation: 'afterIntro'} }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
