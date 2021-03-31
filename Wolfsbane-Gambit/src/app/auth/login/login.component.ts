baa15db3f7351861e4c0746fef7a5ea39db77547
IasminHunTen <constantinIasmin321@yahoo.com>
2021-03-31 18:24:55 +0300

    login page done for now

--- Wolfsbane-Gambit/src/app/auth/login/login.component.ts
+++ Wolfsbane-Gambit/src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormControl, FormGroup,Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userForm: FormGroup;
+  hide=true;
+  constructor(
+    private auth: AuthService
+  ) { }
+
+  
 
   ngOnInit(): void {
+    this.auth.eventAuthError$.subscribe(data=>{
+      console.error(data);
+    })
+    this.userForm=new FormGroup({
+      username: new FormControl("", [Validators.required,Validators.minLength(4),Validators.maxLength(25)]),
+      password: new FormControl("",[Validators.required, Validators.minLength(8),Validators.maxLength(50)])
+    })
+  }
+
+  getUsernameErrors(){
+    if(this.userForm.get('username').hasError('required')){
+      return 'This field is required'
+    }
+    if(this.userForm.get('username').hasError('minlength')){
+      return 'Username must be at least 4 charaters'
+    }
+    if(this.userForm.get('username').hasError('maxlength')){
+      return 'Username must be at most 25 characters'
+    }
+    return '';
+  }
+
+  getPasswordErrors(){
+    if(this.userForm.get('password').hasError('required')){
+      return 'This field is required'
+    }
+    if(this.userForm.get('password').hasError('minlength')){
+      return 'Password must be at least 4 charaters'
+    }
+    if(this.userForm.get('password').hasError('maxlength')){
+      return 'Password must be at most 50 characters'
+    }
+    return '';
+  }
+
+  async login(){
+    await this.auth.login(this.userForm.get('username').value,this.userForm.get('password').value);
+  }
+  
+  goToRegister(){
+    this.auth.goToRegister();
   }
 
 }
