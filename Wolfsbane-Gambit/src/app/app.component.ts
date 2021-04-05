a010ef1aa1e1789926f06376fc310923554d814b
EvoShady <danc1_cosmin@yahoo.com>
2021-04-05 08:52:05 +0300

    Added bg image to login and added visibility to some fields

--- Wolfsbane-Gambit/src/app/app.component.ts
+++ Wolfsbane-Gambit/src/app/app.component.ts
@@ -21,11 +21,15 @@ export class AppComponent implements OnInit{
   constructor(private router: Router) {}
 
   ngOnInit(){
+    this.router.navigate(['login']);
+  }
+/*
+  ngOnInit(){
      this.router.navigate(['introPage']);
     setTimeout(() => {
     this.router.navigate(['login']);
    }, 5500)
 
   }
-  
+  */
 }
