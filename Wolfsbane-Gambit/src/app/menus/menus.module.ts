900d85cae5432a56c29ebc7cedb1a24ac7fbc564
IasminHunTen <constantinIasmin321@yahoo.com>
2021-04-17 18:35:07 +0300

    beggin pattern

--- Wolfsbane-Gambit/src/app/menus/menus.module.ts
+++ Wolfsbane-Gambit/src/app/menus/menus.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MenusRoutingModule } from './menus-routing.module';
 import { PatternsComponent } from './patterns/patterns.component';
 import { PuzzlesComponent } from './puzzles/puzzles.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [PatternsComponent, PuzzlesComponent],
   imports: [
     CommonModule,
-    MenusRoutingModule
+    MenusRoutingModule, 
+    MatCardModule
   ]
 })
 export class MenusModule { }
---
+++ Wolfsbane-Gambit/src/app/models/pattern.ts
@@ -0,0 +1,6 @@
+export interface pattern{
+    preview: string,
+    description: string,
+    url_table: string,
+    patternNumber: number
+}
