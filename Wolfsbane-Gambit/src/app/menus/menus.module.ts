59a977ddcd16364daf532f78db55a3ac693df4f2
EvoShady <danc1_cosmin@yahoo.com>
2021-04-19 14:15:51 +0300

    Added Bg Image, grid, menu and buttons (frontend)

--- Wolfsbane-Gambit/src/app/menus/menus.module.ts
+++ Wolfsbane-Gambit/src/app/menus/menus.module.ts
@@ -5,14 +5,17 @@ import { MenusRoutingModule } from './menus-routing.module';
 import { PatternsComponent } from './patterns/patterns.component';
 import { PuzzlesComponent } from './puzzles/puzzles.component';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [PatternsComponent, PuzzlesComponent],
   imports: [
     CommonModule,
     MenusRoutingModule, 
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatPaginatorModule,
   ]
 })
 export class MenusModule { }
