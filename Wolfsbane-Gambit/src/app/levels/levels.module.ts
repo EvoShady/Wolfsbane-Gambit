cc4e0aa3a4c4b5e02306f513d3338d905fb3512e
EvoShady <danc1_cosmin@yahoo.com>
2021-04-26 15:06:53 +0300

    Added frontend for pattern levels

--- Wolfsbane-Gambit/src/app/levels/levels.module.ts
+++ Wolfsbane-Gambit/src/app/levels/levels.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatternLevelsComponent } from './pattern-levels/pattern-levels.component';
 import { PuzzleLevelsComponent } from './puzzle-levels/puzzle-levels.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { LevelsRoutingModule } from './levels-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [PatternLevelsComponent, PuzzleLevelsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LevelsRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ]
 })
 export class LevelsModule { }
