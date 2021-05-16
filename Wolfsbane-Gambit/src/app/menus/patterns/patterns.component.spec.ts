0287ea0bb70b09259eedc1fc9c4b1b1ccfd6e291
IasminHunTen <constantinIasmin321@yahoo.com>
2021-05-16 22:28:04 +0300

    make tests for patterns component

--- Wolfsbane-Gambit/src/app/menus/patterns/patterns.component.spec.ts
+++ Wolfsbane-Gambit/src/app/menus/patterns/patterns.component.spec.ts
@@ -1,17 +1,32 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { RouterModule } from '@angular/router';
+import { By } from 'protractor';
 import { LevelsService } from 'src/app/services/levels.service';
+import { environment } from 'src/environments/environment';
 
 import { PatternsComponent } from './patterns.component';
 
 describe('PatternsComponent', () => {
   let patternComponent: PatternsComponent;
   let lvs: LevelsService
+  let component: PatternsComponent;
+  let fixture: ComponentFixture<PatternsComponent>;
+ 
+  
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PatternsComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        RouterModule.forRoot([]),
+      ],
+      providers: [ ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PatternsComponent);
+    component = fixture.componentInstance;
   });
 
   beforeEach(() => {
@@ -21,4 +36,10 @@ describe('PatternsComponent', () => {
   it('should create', () => {
     expect(patternComponent).toBeTruthy();
   });
+
+  it('should contain false value',async ()=>{
+    component.length=8;
+    await component.populate();
+    expect(component.activeLinks).toContain(false);
+  })
 });
